bc83bd4f94e4cf79b1be33f937bbe78baa901f70
Manoj M <manumadhu1425@gmail.com>
2023-12-06 11:01:50 +0530

    Adding hero section

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,18 @@
+import "./hero.css";
+
+export default function Hero() {
+  return (
+    <section id="hero-section">
+      <div id="header-div">
+        <p>
+          <span></span>Manoj M
+        </p>
+        <h2>Hello, my name's Manoj.</h2>
+        <h2>I'm a Full Stack Developer</h2>
+      </div>
+      <div id="hero-image-div">
+        {/* <img src={HeroImage} id="hero-image" alt="Hero image" /> */}
+      </div>
+    </section>
+  );
+}
